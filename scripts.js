8e3f35209110e8dd0276a4f651efaea2acefc938
monzwe <172489948+monzwe@users.noreply.github.com>
2026-07-24 22:08:43 +0200

    Implement a basic dark-mode switcher

--- scripts.js
+++ scripts.js
@@ -33,3 +33,23 @@
 
     loadHikes();
 
+
+    document.addEventListener('DOMContentLoaded', () => {
+    const switcher = document.getElementById('modeSwitcher');
+    const currentMode = localStorage.getItem('theme') || 'light';
+
+
+    if (currentMode === 'dark') {
+        document.body.classList.add('dark-mode');
+        switcher.title = 'Jour';
+        
+    }
+
+    switcher.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        const mode = document.body.classList.contains('dark-mode') ? 'dark' : 'light';
+        switcher.title = mode === 'dark' ? 'Jour' : 'Nuit';
+        localStorage.setItem('theme', mode);
+    });
+});
+
