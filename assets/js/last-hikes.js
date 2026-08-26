79fb31c0ad229f03e79a324102b396ca626804a3
monzwe <172489948+monzwe@users.noreply.github.com>
2026-08-26 16:18:52 +0200

    Test JSON feed for last few hikes

---
+++ assets/js/last-hikes.js
@@ -0,0 +1,34 @@
+    const scriptUrl = "https://script.google.com/macros/s/AKfycbxrDnesoYawPLQHClhvAdgQ99RgiN-gSm2uwStbYuDarsovduTptgTzfamoxxQPuR5X/exec"; 
+
+    async function loadHikes() {
+      const container = document.getElementById("cards");
+      container.innerHTML = "<div class='rollout-bar'>...</div>";
+
+      try {
+        const response = await fetch(scriptUrl);
+        const hikes = await response.json();
+
+        container.innerHTML = "";
+
+        hikes.forEach(hike => {
+          const card = document.createElement("div");
+          card.className = "card";
+
+          card.innerHTML = `
+            <ul>
+            <li><strong> ${hike.Date || ""}</strong><br>
+            ${hike.Participants || ""}<br>
+            ${hike.Descriptif || ""}</li>
+            </ul>
+          `;
+
+          container.appendChild(card);
+        });
+
+      } catch (err) {
+        console.error(err);
+        container.innerHTML = "<p>(Merci d'actualiser la page / Please refresh page) </p>";
+      }
+    }
+
+    loadHikes();
